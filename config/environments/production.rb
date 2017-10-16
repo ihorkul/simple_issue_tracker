5cd8f7d1c6965f2ebab9bad9338348fcd501bff8
ihorkul <ihor2405@gmail.com>
2017-10-16 16:23:19 +0300

    Add mailgun to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.host_name = 'young-wave-24089.herokuapp.com'
   config.action_mailer.default_url_options = { host: 'young-wave-24089.herokuapp.com', protocol: 'https://' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'young-wave-24089.herokuapp.com',
+    :authentication => :plain,
+  }
 end
